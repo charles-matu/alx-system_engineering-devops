b1ed3bd34c1efc87eef6c9461cc17876b3708e35
charles-matu <matucharleswachira@gmail.com>
2023-06-19 23:06:23 -0700

    Putchar functon

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints each character of "_putchar" followed by a newline
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	_putchar('_');      /* Print underscore character */
+	_putchar('p');      /* Print 'p' character */
+	_putchar('u');      /* Print 'u' character */
+	_putchar('t');      /* Print 't' character */
+	_putchar('c');      /* Print 'c' character */
+	_putchar('h');      /* Print 'h' character */
+	_putchar('a');      /* Print 'a' character */
+	_putchar('r');      /* Print 'r' character */
+	_putchar('\n');     /* Print newline character */
+
+	return (0);
+}
+
